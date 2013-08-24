2089f23a27f5ad69a092b029372af76005baff7f
Sebastien Cagnon <cagnonsebastien@gmail.com>
2013-08-24 17:02:42 +0000

    ejs renders the index on demand

--- web.js
+++ web.js
@@ -1,21 +1,22 @@
-var express = require('express');
-var fs = require("fs");
+var express = require('express')
+  , fs = require("fs")
+  , http = require('http');
 
 var app = express();
 app.use(express.logger());
-app.use(express.static(__dirname + '/public'));
+app.set('port', process.env.PORT || 8080);
 
 app.configure( function () {
+  console.log('setting view engine');
   app.set('views', __dirname + '/views');
   app.set('view engine', 'ejs');
   app.use("/public", express.static(__dirname + '/public'));
 });
 
 app.get('/', function(request, response) {
-  response.render('index');
+  response.render('index', {});
 });
 
-var port = process.env.PORT || 8080;
-app.listen(port, function() {
-  console.log("Listening on " + port);
+http.createServer(app).listen(app.get('port'), function () {
+  console.log("Listening on " + app.get('port'));
 });
