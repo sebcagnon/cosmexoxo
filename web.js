ef806b57551caa8db62ffe2e40b035bc0c0b639f
Sebastien Cagnon <cagnonsebastien@gmail.com>
2013-08-24 15:47:09 +0000

    node serves directly from public folder of the app

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var fs = require("fs");
 var app = express();
 app.use(express.logger());
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  fs.readfile(__dirname + '/public/index.html', 'utf8', function(err, text) {
+    response.send(text);
+  });
 });
 
 var port = process.env.PORT || 8080;
