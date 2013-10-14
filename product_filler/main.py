00d603fe72760adf39737e0ade3b0772cd7cafb9
sebastien cagnon <cagnonsebastien@gmail.com>
2013-10-14 23:22:54 +0900

    added the connect button

--- product_filler/main.py
+++ product_filler/main.py
@@ -18,17 +18,45 @@ class Application(tk.Frame):
     top.columnconfigure(0, weight=1)
     self.rowconfigure(0, weight=1)
     self.columnconfigure(0, weight=1)
-    # select db connection file
-    self.browseButton = tk.Button(self, text='Browse...',
-      command=self.fileBrowser)
+    # database connection
+    self.setConnectionWidgets()
+      
+  def dataConfigBrowser(self):
+    self.dbConfigFileName.set(askopenfilename())
+    
+  def setConnectionWidgets(self):
+    """ select db connection file and connect button """
+    # text variables
     self.dbConfigFileName = tk.StringVar()
+    self.connectionStatus = tk.StringVar()
+    self.connectionStatus.set('Not Connected')
+    self.connectButtonText = tk.StringVar()
+    self.connectButtonText.set('Connect')
+    
+    # file selection
+    self.browseButton = tk.Button(self, text='Browse...',
+      command=self.dataConfigBrowser)
     self.fileEntry = tk.Entry(self, 
       textvariable=self.dbConfigFileName)
-    self.browseButton.grid(row=0, column=0, rowspan=1)
-    self.fileEntry.grid(row=0, column=1, rowspan=5)
+    self.browseButton.grid(row=0, column=0, columnspan=1)
+    self.fileEntry.grid(row=0, column=1, columnspan=3)
+    
+    # connection
+    self.connectButton = tk.Button(self, 
+      textvariable=self.connectButtonText,
+      command=self.connectToDatabase)
+    self.connectStatusLabel = tk.Label(self, 
+      textvariable=self.connectionStatus)
+    self.connectButton.grid(row=1, column=0)
+    self.connectStatusLabel.grid(row=1, column=1)
       
-  def fileBrowser(self):
-    self.dbConfigFileName.set(askopenfilename())
+  def connectToDatabase(self):
+    if self.connectionStatus.get() == 'Not Connected':
+      self.connectionStatus.set('Connected')
+      self.connectButtonText.set('Disconnect')
+    elif self.connectionStatus.get() == 'Connected':
+      self.connectionStatus.set('Not Connected')
+      self.connectButtonText.set('Connect')
     
 app = Application()
 app.master.title('Product Filler')
