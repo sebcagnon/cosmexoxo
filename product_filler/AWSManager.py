6898390e308c687411d2532ea9a686efd814b917
sebastien cagnon <cagnonsebastien@gmail.com>
2014-03-08 16:39:58 +0900

    #1: implemented getImageFromName and deleteKey

--- product_filler/AWSManager.py
+++ product_filler/AWSManager.py
@@ -27,22 +27,102 @@ class AWSManager(object):
       self.bucket = None
       self.connectionStatus = self.DISCONNECTED
 
+  def uploadImage(self, keyName, imagePath, **metadata):
+    """Uploads the image from file to bucket"""
+    if self.connectionStatus != self.CONNECTED:
+      raise AWSManagerError('Cannot upload image if not connected')
+    key = self.bucket.new_key(keyName)
+    for k,v in metadata.items():
+      key.set_metadata(k, v)
+    key.set_contents_from_filename(imagePath)
+
+  def getImageFromName(self, keyName, destinationFile):
+    """Returns True if image was downloaded to destFile, False otherwise"""
+    if self.connectionStatus != self.CONNECTED:
+      raise AWSManagerError('Cannot download image if not connected')
+    key = self.bucket.get_key(keyName)
+    if key:
+      key.get_contents_to_filename(destinationFile)
+      return True
+    else:
+      return False
+
+  def deleteKey(self, keyName):
+    """Deletes keyName key"""
+    if self.connectionStatus != self.CONNECTED:
+      raise AWSManagerError('Cannot delete key image if not connected')
+    self.bucket.delete_key(keyName)
+
+
+class AWSManagerError(BaseException):
+  """Error raised when trying to use a disconnected AWSManager"""
+
+  def __init__(self, value):
+    self.value = value
+
+  def __str__(self):
+    return repr(self.value)
+
 
 if __name__=='__main__':
   import argparse
   import json
+  import hashlib
+  import os
   parser = argparse.ArgumentParser(description='Tests for AWSManager')
   parser.add_argument('jsonFile', type=argparse.FileType('r'))
   args = parser.parse_args()
   data = json.load(args.jsonFile)
-  awsMgr = AWSManager(data['s3_bucket'], **data['aws_access_key'])
-  awsMgr.connect()
-  assert awsMgr.connectionStatus == awsMgr.CONNECTED, 'Connection failure'
-  print 'Connected'
-  awsMgr.disconnect()
-  assert awsMgr.connectionStatus == awsMgr.DISCONNECTED, \
-                       'Disconnection failure'
-  print 'Disconnected'
+  try:
+    # Testing creation and connection
+    awsMgr = AWSManager(data['s3_bucket'], **data['aws_access_key'])
+    awsMgr.connect()
+    assert awsMgr.connectionStatus == awsMgr.CONNECTED, 'Connection failure'
+    print 'Connected'
+
+    # Testing upload and download image
+    testImgFileName = 'resources/placeholder.png'
+    fileName2 = 'resources/placeholder2.png'
+    originalHash = hashlib.sha256(open(testImgFileName, 'rb').read()).digest()
+    testKey = 'test_img'
+    metadata = {'title':'test'}
+    assert awsMgr.getImageFromName('dfklajfnejcdjka', fileName2) == False, \
+                         'getImageFromName should return false for fake keys'
+    awsMgr.uploadImage(testKey, testImgFileName, **metadata)
+    key = awsMgr.bucket.get_key(testKey)
+    assert key != None, 'uploadImage: key was not created'
+    assert key.metadata == metadata, 'uploadImage metadata was not saved'
+    assert awsMgr.getImageFromName(testKey, fileName2) == True, \
+                         'getImageFromName should return True on success'
+    newHash = hashlib.sha256(open(fileName2, 'rb').read()).digest()
+    assert newHash == originalHash, 'image was modified between upload ' \
+                                     'and download'
+    awsMgr.deleteKey(testKey)
+    key = awsMgr.bucket.get_key(testKey)
+    assert key == None, 'deleteKey did not delete the key'
+
+    # Testing disconnection handling
+    awsMgr.disconnect()
+    assert awsMgr.connectionStatus == awsMgr.DISCONNECTED, \
+                         'Disconnection failure'
+    print 'Disconnected'
+    try:
+      awsMgr.uploadImage(testKey, testImgFileName, **metadata)
+      assert None, 'uploadImage should have raised'
+    except AWSManagerError:
+      pass
+    try:
+      awsMgr.getImageFromName(testKey, fileName2)
+      assert None, 'getImageFromName should have raised'
+    except AWSManagerError:
+      pass
+    try:
+      awsMgr.deleteKey(testKey)
+      assert None, 'deleteKey should have raised'
+    except AWSManagerError:
+      pass
+  finally:
+    if os.path.exists(fileName2):
+      os.remove(fileName2)
+
   print 'All Tests Passed'
-  
-  
