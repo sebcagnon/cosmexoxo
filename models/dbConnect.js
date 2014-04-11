42150038830dbc863c04b82eadb30e948541d0e0
sebastien cagnon <cagnonsebastien@gmail.com>
2014-04-12 01:08:39 +0900

    #12: implemented getProduct which returns a Product object

--- models/dbConnect.js
+++ models/dbConnect.js
@@ -1,5 +1,7 @@
 var pg = require('pg');
 var fs = require('fs');
+var async = require('async');
+var _ = require('underscore');
 
 // initializing db values
 
@@ -30,48 +32,104 @@ var config = {
   ssl: true
 }
 
-var schema = "product_info"
-
 module.exports = {
 
+  // Get product info from db and format it into a nice Product object
   getProduct : function (productId, onSuccess, onError) {
-    pg.connect(config, function(err, client, done) {
-      if (err) {
-        onError(err);
-      } else {
-        str = "SELECT p.product_id, p.name, p.description, p.active,\
-        b.brand_id, b.name as brand_name, c.company_id,\
-        c.name as company_name\
-        FROM SCHEMA.product p\
-        INNER JOIN SCHEMA.brand b ON p.brand_id = b.brand_id\
-        INNER JOIN SCHEMA.company c ON b.company_id = c.company_id\
-        WHERE p.product_id = $1"
-        newStr = replaceAll('SCHEMA', schema, str);
-        client.query(newStr, [productId], function (err, result) {
-          if (err) {
-            onError(err);
-          } else {
-            onSuccess(result);
-          }
-          done();
-        });
-      }
-    });
+    async.parallel([
+        async.apply(getProductBasics, productId),
+        async.apply(getProductCategories, productId),
+        async.apply(getProductVariants, productId)
+      ],
+      function(err, result) {
+        if (err) return onError(err);
+        var product = createProduct(result)
+        onSuccess(product);
+      })
   },
 
-  getProducts : function () {
-    pg.connect(config, function(err, client, done) {
-      if(err) return console.error(err);
-      client.query('SELECT * FROM product_info.product', function(err, result) {
-    	  done();
-    	  if(err) return console.error(err);
-    	  console.log(result.rows);
-      });
-  	pg.end()
-    });
+  // Closes remaining connections to the database
+  // after queries have all returned
+  // to allow for programs to finish nicely.
+  close : function () {
+    pg.end();
   }
 
 } // end of exported functions
+
+
+// query functions meant to be used in async
+
+// Fetch basic info directly available from the Product table
+function getProductBasics(productId, callback) {
+  pg.connect(config, function onConnected(err, client, done) {
+    if (err) return callback(err);
+    var str = "SELECT p.product_id, p.name, p.description, p.active,\
+      b.brand_id, b.name as brand_name, c.company_id,\
+      c.name as company_name\
+      FROM SCHEMA.product p\
+      INNER JOIN SCHEMA.brand b ON p.brand_id = b.brand_id\
+      INNER JOIN SCHEMA.company c ON b.company_id = c.company_id\
+      WHERE p.product_id = $1";
+    var newStr = replaceAll('SCHEMA', 'product_info', str);
+    client.query(newStr, [productId], function onQueryFinished(err, result) {
+      callback(err, result.rows);
+      done();
+    });
+  });
+}
+
+// Fetch list of categories associated with the Product
+function getProductCategories(productId, callback) {
+  pg.connect(config, function onConnected(err, client, done) {
+    if (err) return callback(err);
+    var str = "SELECT DISTINCT p.product_id, cat.category_id, cat.name\
+      FROM SCHEMA.product p\
+      INNER JOIN SCHEMA.product_category pc ON p.product_id = pc.product_id\
+      INNER JOIN SCHEMA.category cat ON cat.category_id = pc.category_id\
+      WHERE p.product_id = $1"
+    var newStr = replaceAll('SCHEMA', 'product_info', str);
+    client.query(newStr, [productId], function onQueryFinished(err, result) {
+      callback(err, result.rows);
+      done();
+    });
+  });
+}
+
+// Fetch list of variants associated with the Product
+function getProductVariants(productId, callback) {
+  pg.connect(config, function onConnected(err, client, done) {
+    if (err) return callback(err);
+    var str = "SELECT p.product_id, v.variant_id, v.name, v.price, v.weight\
+      FROM SCHEMA.product p\
+      INNER JOIN SCHEMA.variant v ON p.product_id = v.product_id\
+      WHERE p.product_id = $1";
+    var newStr = replaceAll('SCHEMA', 'product_info', str);
+    client.query(newStr, [productId], function onQueryFinished(err, result) {
+      callback(err, result.rows);
+      done();
+    });
+  });
+}
+
+// Creates a nice Product object to use in webpage from async result
+function createProduct(result) {
+  var basics = result[0][0];
+  var product = {
+    product_id: basics.product_id,
+    name: basics.name,
+    brand_name: basics.brand_name,
+    company_name: basics.company_name,
+    categories: _.pluck(result[1], 'name'),
+    variants: _.map(result[2], function selector(variant) {
+                return {variant_id: variant.variant_id,
+                        name: variant.name,
+                        price: variant.price,
+                        weight: variant.weight}
+              })
+  };
+  return product;
+}
 
 // helper functions
 
--- tests/test_dbConnect.js
+++ tests/test_dbConnect.js
@@ -1,11 +1,9 @@
 var dbConnect = require('../models/dbConnect.js')
 
-console.log("Testing getProducts");
-dbConnect.getProducts();
 console.log("Testing getProduct");
 successCb = function(result) {
   console.log("Success!");
-  console.log(result.rows);
+  console.log(result);
 }
 errorCb = function(error) {
   console.log("Error!");
@@ -13,3 +11,6 @@ errorCb = function(error) {
 }
 dbConnect.getProduct(24, successCb, errorCb);
 dbConnect.getProduct(16, successCb, errorCb);
+
+console.log('Finished!');
+dbConnect.close();
