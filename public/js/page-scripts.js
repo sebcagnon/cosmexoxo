b689dedb1e2a29ec82eb4d14906f04738743d0f0
sebastien cagnon <cagnonsebastien@gmail.com>
2014-04-26 20:30:26 +0900

    finished navbar with submenus, closes #24

---
+++ public/js/page-scripts.js
@@ -0,0 +1,24 @@
+$('ul.dropdown-menu [data-toggle=dropdown]').on('click', function(event) {
+  // Avoid following the href location when clicking
+  event.preventDefault();
+  // Avoid having the menu to close when clicking
+  event.stopPropagation();
+  // If a menu is already open we close it
+  if ($(this).parent().hasClass('open'))
+    return $(this).parent().removeClass('open');
+
+  $(this).parent().siblings().removeClass('open');
+
+  // opening the one you clicked on
+  $(this).parent().addClass('open');
+
+  var menu = $(this).parent().find("ul");
+  var menupos = menu.offset();
+
+  if ((menupos.left + menu.width()) + 30 > $(window).width()) {
+    var newpos = - menu.width();
+  } else {
+    var newpos = $(this).parent().width();
+  }
+  menu.css({ left:newpos });
+});
