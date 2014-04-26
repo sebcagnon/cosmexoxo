bccbed12296c0c878c6528747f5e83a98a37ebee
sebastien cagnon <cagnonsebastien@gmail.com>
2014-04-27 02:00:18 +0900

    improved thumbnails with better columns and max img size, and improved categories page by using the 2 columns class

--- models/utils.js
+++ models/utils.js
@@ -10,15 +10,23 @@ var utils = {
   },
 
   // returns the HTML for displaying the category tree
-  displayCategoryTree : function (categoryTree, n) {
+  displayCategoryTree : function (categoryTree, n, isMain) {
     if (categoryTree.length == 0) return '';
     var space = Array(n).join('  ');
-    var resultStr = '\n' + space + '<ul>';
+    if (isMain == 1) {
+      var resultStr = '\n' + space + '<ul class="columns2">';
+    } else {
+      var resultStr = '\n' + space + '<ul>';
+    }
     for (var i=0; i<categoryTree.length; i++) {
       resultStr += '\n' + space + '  <li>';
+      if (isMain == 0) resultStr += '<h2>'
       resultStr += utils.link_to('/categories/' + categoryTree[i].name,
                                  categoryTree[i].name);
-      resultStr += utils.displayCategoryTree(categoryTree[i].children, n+2);
+      if (isMain == 0) resultStr += '</h2>'
+      resultStr += utils.displayCategoryTree(categoryTree[i].children,
+                                             n+2,
+                                             isMain+1);
       resultStr += '\n' + space + '  </li>'
     }
     resultStr += '\n' + space + '</ul>';
--- public/js/page-scripts.js
+++ public/js/page-scripts.js
@@ -1,24 +1,33 @@
-$('ul.dropdown-menu [data-toggle=dropdown]').on('click', function(event) {
+$('ul.dropdown-menu [data-toggle=dropdown]').on('click mouseenter',
+                                                function openMenu (event) {
   // Avoid following the href location when clicking
   event.preventDefault();
   // Avoid having the menu to close when clicking
   event.stopPropagation();
-  // If a menu is already open we close it
-  if ($(this).parent().hasClass('open'))
-    return $(this).parent().removeClass('open');
-
-  $(this).parent().siblings().removeClass('open');
-
-  // opening the one you clicked on
-  $(this).parent().addClass('open');
-
-  var menu = $(this).parent().find("ul");
-  var menupos = menu.offset();
-
-  if ((menupos.left + menu.width()) + 30 > $(window).width()) {
-    var newpos = - menu.width();
-  } else {
-    var newpos = $(this).parent().width();
+  var parent = $(this).parent();
+  if (event.type == 'click') {
+    // If a menu is already open we close it
+    if (parent.hasClass('open'))
+        return parent.removeClass('open');
+    parent.addClass('open');
+  } else if (event.type == 'mouseenter') {
+    if (!parent.hasClass('open'))
+        parent.addClass('open');
   }
-  menu.css({ left:newpos });
+  if (parent.hasClass('open')) {
+    parent.siblings().removeClass('open');
+
+    // opening the one you clicked on
+    parent.addClass('open');
+
+    var menu = $(this).parent().find("ul");
+    var menupos = menu.offset();
+
+    if ((menupos.left + menu.width()) + 30 > $(window).width()) {
+    var newpos = - menu.width() + 5;
+    } else {
+    var newpos = parent.width() - 5;
+    }
+    menu.css({ left:newpos });
+}
 });
